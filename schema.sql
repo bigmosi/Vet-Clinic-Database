b4abbad0c8736efb3da378c6279c175b6f0ad131
bigmosi <kinyeramo@gmail.com>
2022-08-10 14:58:16 +0300

    update database

--- schema.sql
+++ schema.sql
@@ -1,10 +1,16 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-    id INT GENERATED ALWAYS AS IDENTITY, 
-    name varchar(100), 
-    date_of_birth date, 
-    escape_attempts int, 
-    neutered bool, 
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts int,
+    neutered bool,
     weight_kg decimal
 );
+
+-- UPDATE TABLE WITH NEW COLUMN - DAY 2
+
+ALTER TABLE animals
+  ADD species varchar(100);
+
